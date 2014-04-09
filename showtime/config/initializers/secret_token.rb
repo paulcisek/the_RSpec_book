92cdef570520ffdd1e402190f008daf90847d5fb
paulcisek <paul.cisek@outlook.com>
2014-04-09 12:31:26 +0200

    showtime rails app with 1st passing scenario

---
+++ showtime/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showtime::Application.config.secret_key_base = '9d949aa6d7dc99d1cbd0773b15b52c6a1afe9e7ad8251d7095f370cc84fa1225edbac1c43fc2be6b3888e2c90c8784b7be0a849b674d4db9a6efa362c5ddf995'
