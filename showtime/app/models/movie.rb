4a0f5cc5297d20a27b3ab7d9b593e3ed91417c3b
paulcisek <paul.cisek@outlook.com>
2014-04-09 13:59:13 +0200

    showtime rails app with 2 passing scenario

--- showtime/app/models/movie.rb
+++ showtime/app/models/movie.rb
@@ -8,6 +8,7 @@ class Movie < ActiveRecord::Base
   end
 
   def formatted_time
-    showtime_time.strftime("%l:%M%p").strip.downcase
+    formatted_string = showtime_time.min.zero? ? "%l%p" : "%l:%M%p"
+    showtime_time.strftime(formatted_string).strip.downcase
   end
 end
